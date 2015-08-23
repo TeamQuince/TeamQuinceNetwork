f8904d0b8a88e079ab8ebd13587ec25a814330d3
idmitrov <mailto.dimitrov@gmail.com>
2015-08-23 20:40:20 +0300

    Adding BaseApiController

---
+++ TeamQuinceSocialNetwork/TQSN.Services/Controllers/BaseApiController.cs
@@ -0,0 +1,20 @@
+﻿namespace TQSN.Services.Controllers
+{
+    using System.Web.Http;
+    using Data;
+
+    public class BaseApiController : ApiController
+    {
+        public BaseApiController() : this(new ApplicationDbContext())
+        {
+        }
+
+        public BaseApiController(ApplicationDbContext data)
+        {
+            this.Data = data;
+        }
+
+        public ApplicationDbContext Data { get; set; }
+    }
+}
+
