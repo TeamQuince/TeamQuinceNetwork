4e73b631b1faceaf9f5e1a0afd73c7760d97e3a6
straho99 <strahil.ruychev@gmail.com>
2015-08-25 14:57:10 +0300

    Added some code to test the models

--- InheritanceTest/SocialNetwork.ConsoleClient/SocialNetworkApp.cs
+++ InheritanceTest/SocialNetwork.ConsoleClient/SocialNetworkApp.cs
@@ -11,8 +11,81 @@
     {
         public static void Main()
         {
+
+            AddUsers();
+            PostToUser(1, 2, "Hi Plami, Hi are you?");
+            CommentToUserPost(2, 1, "I am fine, thank you.");
+            LikeUserPost(2, 1);
+        }
+
+        public static void AddUsers()
+        {
             var context = new SocialNetworkContext();
-            Console.WriteLine(context.Groups.Count());
+            if (context.Users.Count() > 0)
+            {
+                return;
+            }
+            var straho = new User()
+            {
+                Name = "Strahil",
+                Username = "straho"
+            };
+            var plamena = new User()
+            {
+                Name = "Plamena",
+                Username = "plami"
+            };
+            context.Users.Add(plamena);
+            context.SaveChanges();
+        }
+
+        public static void PostToUser(int fromUserId, int toUserId, string content)
+        {
+            var context = new SocialNetworkContext();
+
+            var sender = context.Users.Find(fromUserId);
+            var recipient = context.Users.Find(toUserId);
+            var post = new UserPost()
+            {
+                Content = content,
+                Author = sender,
+                PostedOn = DateTime.Now,
+                User = recipient
+            };
+            context.UserPosts.Add(post);
+            context.SaveChanges();
+        }
+
+        public static void CommentToUserPost(int fromUserId, int toUserPostId, string content)
+        {
+            var context = new SocialNetworkContext();
+
+            var sender = context.Users.Find(fromUserId);
+            var userPost = context.UserPosts.Find(toUserPostId);
+            var comment = new UserPostComment()
+            {
+                Content = content,
+                Author = sender,
+                PostedOn = DateTime.Now,
+                UserPost = userPost
+            };
+            context.UserPostComments.Add(comment);
+            context.SaveChanges();
+        }
+
+        public static void LikeUserPost(int userId, int userPostId)
+        {
+            var context = new SocialNetworkContext();
+
+            var user = context.Users.Find(userId);
+            var post = context.UserPosts.Find(userPostId);
+            var like = new UserPostLike()
+            {
+                Author = user,
+                Post = post
+            };
+            context.UserPostLikes.Add(like);
+            context.SaveChanges(); 
         }
     }
 }
---
+++ InheritanceTest/SocialNetwork.Models/GroupPostComment.cs
@@ -0,0 +1,36 @@
+﻿namespace SocialNetwork.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class GroupPostComment : Posting
+    {
+        private ICollection<GroupPostCommentLike> likes;
+
+        public GroupPostComment()
+        {
+            this.likes = new HashSet<GroupPostCommentLike>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int GroupPostId { get; set; }
+
+        public virtual GroupPost GroupPost { get; set; }
+
+        public virtual ICollection<GroupPostCommentLike> Likes 
+        {
+            get
+            {
+                return this.likes;
+            }
+
+            set
+            {
+                this.likes = value;
+            }
+        }
+    }
+}
